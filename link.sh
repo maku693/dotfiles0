d39fd296ce4f73df4ba46aa84a003758a0f985ec
maku693 <maku3ts@gmail.com>
2016-01-10 23:35:12 +0900

    Fix link.sh to handle git related files correctly

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd `dirname "${0}"`
-files=`find . -type f | sed -e "s/.\///" | grep -v "^\.git" | grep "^\."`
+files=`find . -type f | sed -e "s/.\///" | grep -v "^\.git\/" | grep "^\."`
 for f in ${files}; do
     ln -si "${PWD}/${f}" "${HOME}/${f}" && echo "${PWD}/${f}" "${HOME}/${f}"
 done
