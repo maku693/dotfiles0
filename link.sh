126d1f5fc1c8087a8203c66b28e8317daa72009f
maku693 <maku3ts@gmail.com>
2016-01-10 23:16:19 +0900

    Fix indent

--- link.sh
+++ link.sh
@@ -2,5 +2,5 @@
 cd `dirname "${0}"`
 files=`find . -type f | sed -e "s/.\///" | grep -v "^\.git" | grep "^\."`
 for f in ${files}; do
-	ln -si "${PWD}/${f}" "${HOME}/${f}" && echo "${PWD}/${f}" "${HOME}/${f}"
+    ln -si "${PWD}/${f}" "${HOME}/${f}" && echo "${PWD}/${f}" "${HOME}/${f}"
 done
